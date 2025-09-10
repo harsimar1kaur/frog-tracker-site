c514cf611db4cb1d8e97ba2b4e833220919cb1d1
Harsimar Kaur <harsimar.jk@gmail.com>
2025-09-10 02:44:01 -0700

    navbar

---
+++ frog-app/src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react' 
+
+
+export default function Navbar() {
+  return (
+    <>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <Link to="/" className="navBar-logo">
+                TRVL
+                </Link>
+
+            </div>
+        </nav>
+      
+    </>
+  )
+}
