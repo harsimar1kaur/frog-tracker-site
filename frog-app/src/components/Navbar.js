db95eb0d18cea68ad435332e82bcc3c135bbd222
Harsimar Kaur <harsimar.jk@gmail.com>
2025-09-12 02:42:05 -0700

    Added navbar links

--- frog-app/src/components/Navbar.js
+++ frog-app/src/components/Navbar.js
@@ -9,6 +9,28 @@ const Navbar = () => {
   <div className="navbar-left"> 
     <img src={frogLogo} alt="FrogTracker Logo" className="logo-img" />
   </div>
+  
+  <div className='navbar-center'>
+    <ul className="nav-links">
+      <li>
+        <a href="/faq">HOME</a>
+      </li>
+      <li>
+        <a href="/help"> ABOUT </a>
+      </li>
+       <li>
+        <a href="/help"> CONTACT </a>
+      </li>
+    </ul>
+  </div>
+
+  <div className='navbar-right'>
+    <ul className="nav-links2">
+      <li>
+        <a href="/faq">SIGN IN</a>
+      </li>
+    </ul>
+  </div>
 </nav>
 );
 };
