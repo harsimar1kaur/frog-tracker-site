b13f6173f65e17fdd5a8af92c74fe9c5ea16bf5d
Harsimar Kaur <harsimar.jk@gmail.com>
2025-09-22 04:47:58 -0700

    cleaned up tracker page and added boxes to create sections

--- frog-app/src/pages/tracker.jsx
+++ frog-app/src/pages/tracker.jsx
@@ -1,12 +1,14 @@
 import './tracker.css';
 
-
 export default function Tracker() {
-    return (
-    <div className='tracker-page'>
-        <h1>
-            Tracker
-        </h1>
+  return (
+    <div className="tracker-page">
+      <div className="tracker-left">
+        Left Vertical Box
+      </div>
+      <div className="tracker-right">
+        Right Square Box
+      </div>
     </div>
-    );
+  );
 }
