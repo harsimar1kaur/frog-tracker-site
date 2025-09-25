b0163971c54df17bce1191088986cb00d85faa45
Harsimar Kaur <harsimar.jk@gmail.com>
2025-09-25 01:20:51 -0700

    Added background images/design to main tracker page

--- frog-app/src/pages/tracker.jsx
+++ frog-app/src/pages/tracker.jsx
@@ -1,14 +1,16 @@
 import './tracker.css';
+import frogImg from '../assets/Frogicon.png';
 
 export default function Tracker() {
   return (
     <div className="tracker-page">
-      <div className="tracker-left">
-        Left Vertical Box
+      <div className="tracker-left">Tracker</div>
+
+      <div className="tracker-middle">
+        <img src={frogImg} alt="Decoration" className="tracker-image" />
       </div>
-      <div className="tracker-right">
-        Right Square Box
-      </div>
+
+      <div className="tracker-right">Frog</div>
     </div>
   );
 }
