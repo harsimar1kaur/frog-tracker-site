f562ba3b76da214fcc5bd7a69c17a46d9d5d96a0
Harsimar Kaur <harsimar.jk@gmail.com>
2025-10-10 04:34:24 -0700

    styled Add Task popup with text input and highlight interactions but there is no saving yet

--- frog-app/src/pages/tracker.jsx
+++ frog-app/src/pages/tracker.jsx
@@ -10,20 +10,49 @@ export default function Tracker() {
   return (
       <div className="tracker-page">
       <div className="tracker-left">
+        
         <div className="tasks-header">
-          <h2 className="task-title">Your Tasks
-          </h2>
-          <button className="green-btn" onClick={() => setIsModalOpen(true)}>
-            +
-          </button>
+          <h2 className="task-title">Your Tasks</h2>
+          <button className="plus-btn" onClick={() => setIsModalOpen(true)}>+</button>
+          </div>
+            
             <Modal isOpen={isModalOpen} onClose={() => setIsModalOpen(false)}>
-              <h2>Add Task</h2>
-              <button onClick={() => setIsModalOpen(false)}>Close</button>
-            </Modal>
-        </div>
+              
+              <h2 className="modal-title">Add Task</h2>
+              
+              
+              <form className="task-form">
+                
+                <label htmlFor="taskTitle">Task Title</label>
+                <input
+                id="taskTitle"
+                name="taskTitle"
+                type="text"
+                placeholder='Name your task!'
+                />
 
-        <ul className="task-list">
-        </ul>
+                <label htmlFor="taskDescr">Describe Your Task</label>
+                <textarea
+                id="taskDescr"
+                name="taskDescr"
+                type="text"
+                placeholder='Describe your task!'
+                rows="5"
+                />
+
+                <div className="priority-selector">
+                  <span className="priority-option">Low</span>
+                  <span className="priority-option">Medium</span>
+                  <span className="priority-option">High</span>
+                </div>
+
+                <div className="form-buttons">
+                <button onClick={() => setIsModalOpen(false)}>Cancel</button>
+                <button onClick={() => setIsModalOpen(false)}>Add Task</button>
+                </div>
+              
+              </form>
+            </Modal>
       </div>
 
       <div className="tracker-middle">
