f18ed7e44acfb2cffb32bd5358df81065b70f4d5
Harsimar Kaur <harsimar.jk@gmail.com>
2026-04-10 00:50:41 -0700

    UpdatedReadme

--- frog-app/src/pages/home.jsx
+++ frog-app/src/pages/home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
     <div className='home-page'>
         <h1>Hop towards greatness</h1>
-        <h2>Click the frog in the top right corner to access your tracker!</h2>
+        <h2>Click the frog in the top left corner to access your tracker!</h2>
         
     </div>
     );
